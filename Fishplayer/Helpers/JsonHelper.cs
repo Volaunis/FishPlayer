1a8ed3dcc41bc3f54aace5af0cc797a31da9d254
Allan Kjærgaard <Allan Kjrgaard>
2016-10-19 18:05:42 +0200

    Changes

--- Fishplayer/Helpers/JsonHelper.cs
+++ Fishplayer/Helpers/JsonHelper.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Fishplayer.Helpers
 {
-    class JsonHelper
+    public class JsonHelper
     {
+        public static string Serialize<T>(T item) where T : class
+        {
+            return JsonConvert.SerializeObject(item);
+        }
 
+        public static T DeSerialize<T>(string value) where T : class
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
     }
 }
--- Fishplayer/Models/PlayerConfig.cs
+++ Fishplayer/Models/PlayerConfig.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fishplayer.Helpers;
+using System.IO;
 
 namespace Fishplayer.Models
 {
     public class PlayerConfig
     {
+
         public float Volume { get; set; }
         public string DirectoryPath { get; set; }
 
+        public void Save()
+        {
+            var data = JsonHelper.Serialize<PlayerConfig>(this);
+            File.WriteAllText(getConfigFile(), data);
+        }
+
+        public static PlayerConfig Load()
+        {
+
+            var data = File.ReadAllText(getConfigFile());
+            return JsonHelper.DeSerialize<PlayerConfig>(data);
+        }
+
+        private static string getConfigFile()
+        {
+            return Path.Combine(Environment.CurrentDirectory, "settings.json");
+        }
     }
 }
