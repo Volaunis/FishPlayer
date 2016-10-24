d6517e89aa2822d5c72d8d65e172646ecefdf5d2
Allan Kjærgaard <Allan Kjrgaard>
2016-10-24 18:55:41 +0200

    Changes

---
+++ Fishplayer/Helpers/ConfigHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fishplayer.Models;
+using System.IO;
+
+namespace Fishplayer.Helpers
+{
+    public class ConfigHelper
+    {
+        public static void Save(PlayerConfig config)
+        {
+            var data = JsonHelper.Serialize<PlayerConfig>(config);
+            File.WriteAllText(getConfigFile(), data);
+        }
+
+        public static PlayerConfig Load()
+        {
+
+            var data = File.ReadAllText(getConfigFile());
+            return JsonHelper.DeSerialize<PlayerConfig>(data);
+        }
+
+        private static string getConfigFile()
+        {
+            return Path.Combine(Environment.CurrentDirectory, "settings.json");
+        }
+    }
+}
--- Fishplayer/Models/PlayerConfig.cs
+++ Fishplayer/Models/PlayerConfig.cs
@@ -14,22 +14,6 @@ namespace Fishplayer.Models
         public float Volume { get; set; }
         public string DirectoryPath { get; set; }
 
-        public void Save()
-        {
-            var data = JsonHelper.Serialize<PlayerConfig>(this);
-            File.WriteAllText(getConfigFile(), data);
-        }
-
-        public static PlayerConfig Load()
-        {
-
-            var data = File.ReadAllText(getConfigFile());
-            return JsonHelper.DeSerialize<PlayerConfig>(data);
-        }
-
-        private static string getConfigFile()
-        {
-            return Path.Combine(Environment.CurrentDirectory, "settings.json");
-        }
+        public PlaylistHelper playlistHelper; 
     }
 }
